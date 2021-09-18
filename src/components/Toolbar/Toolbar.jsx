352a2809e73e67abfa7825b61079b94a1bc36f63
Alyssa <alysurr@Alyssas-MacBook-Pro.local>
2021-09-18 13:09:52 -0400

    added buttons for toolbar

--- src/components/Toolbar/Toolbar.jsx
+++ src/components/Toolbar/Toolbar.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Toolbar = () => {
     return (
-        <div>
+        <>
             
-        </div>
+        </>
     )
 }
 
--- src/pages/CanvasPage/CanvasPage.jsx
+++ src/pages/CanvasPage/CanvasPage.jsx
@@ -4,15 +4,20 @@ import rough from 'roughjs/bundled/rough.esm';
 
 const generator = rough.generator();
 
-function createElement(x1, y1, x2, y2) {
-  const roughElement = generator.line(x1, y1, x2, y2);
+function createElement(x1, y1, x2, y2, type) {
+	const roughElement =
+		type === "line"
+			? generator.line(x1, y1, x2, y2)
+			: generator.rectangle(x1, y1, x2 - x1, y2 - y1);
+		// generator.circle(80, 120, 50);;
   return {x1, y1, x2, y2, roughElement};
 }
 
 const CanvasPage = () => {
 
   const [elements, setElements] = useState([]);
-  const [drawing, setDrawing] = useState(false);
+	const [drawing, setDrawing] = useState(false);
+	const [elementType, setElementType] = useState("line")
 
   useLayoutEffect(() => {
     const canvas = document.getElementById("canvas");
@@ -29,7 +34,7 @@ const CanvasPage = () => {
 
     const { clientX, clientY } = e;
 
-    const element = createElement(clientX, clientY, clientX, clientY);
+    const element = createElement(clientX, clientY, clientX, clientY, elementType);
     setElements((prevState) => [...prevState, element]);
   };
   
@@ -39,7 +44,7 @@ const CanvasPage = () => {
     const { clientX, clientY } = e;
     const index = elements.length - 1;
     const { x1, y1 } = elements[index];
-    const updatedElement = createElement(x1, y1, clientX, clientY);
+    const updatedElement = createElement(x1, y1, clientX, clientY, elementType);
 
     const elementsCopy = [...elements];
     elementsCopy[index] = updatedElement;
@@ -51,8 +56,37 @@ const CanvasPage = () => {
   };
 
   return (
-    <>
-      
+		<>
+			{/* Toolbar Component */}
+			<div className="toolbar">
+				<input
+					type="radio"
+					id="line"
+					checked={elementType === "line"}
+					onChange={() => setElementType("line")}
+					className="tool"
+				/>
+				<label
+					htmlFor="line"
+					className="tool__label"
+				>
+					Line
+				</label>
+				<input
+					type="radio"
+					id="rectangle"
+					checked={elementType === "rectangle"}
+					onChange={() => setElementType("rectangle")}
+					className="tool"
+				/>
+				<label
+					htmlFor="rectangle"
+					className="tool__label"
+				>
+					Rectangle
+				</label>
+			</div>
+			{/* Canvas Component */}
       <canvas
         id="canvas"
         width={window.innerWidth}
