f3d3463604de74906e3f8cbd231786de69dc6fba
Alyssa <alysurr@Alyssas-MacBook-Pro.local>
2021-09-19 14:14:27 -0400

    added about page and got fonts to work

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,9 +1,89 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './AboutPage.scss';
+import home from '../../assets/images/home.png';
+import back from '../../assets/images/arrow.png'
 
 export default function AboutPage() {
     return (
-        <div>
+        <div className="about">
+            <h1>About Drawllab</h1>
+            <p className="about__text">Drawllab is a browser based whiteboard and drawing application. Multiple users can use Drawllab to work on the same canvas simultaneously.</p>
+
+            <h1>Key</h1>
+            <div className="key">
+                <ul className="key__ul">
+                    <li className="key__li">
+                        <img src="https://via.placeholder.com/50" alt="key" className="key__icon" />
+                        <p>Select Color</p>
+                    </li>
+                    <li className="key__li">
+                        <img src="https://via.placeholder.com/50" alt="key" className="key__icon" />
+                        <p>Pen/Brush</p>
+                    </li>
+                    <li className="key__li">
+                        <img src="https://via.placeholder.com/50" alt="key" className="key__icon" />
+                        <p>Eraser</p>
+                    </li>
+                    <li className="key__li">
+                        <img src="https://via.placeholder.com/50" alt="key" className="key__icon" />
+                        <p>Draw Straight Line</p>
+                    </li>
+                    <li className="key__li">
+                        <img src="https://via.placeholder.com/50" alt="key" className="key__icon" />
+                        <p>Create Quadrilateral</p>
+                    </li>
+                    <li className="key__li">
+                        <img src="https://via.placeholder.com/50" alt="key" className="key__icon" />
+                        <p>Create Circle</p>
+                    </li>
+                    <li className="key__li">
+                        <img src="https://via.placeholder.com/50" alt="key" className="key__icon" />
+                        <p>Select</p>
+                    </li>
+                    <li className="key__li">
+                        <img src="https://via.placeholder.com/50" alt="key" className="key__icon" />
+                        <p>Resize</p>
+                    </li>
+                    <li className="key__li">
+                        <img src="https://via.placeholder.com/50" alt="key" className="key__icon" />
+                        <p>Generate Collab Link</p>
+                    </li>
+                    <li className="key__li">
+                        <img src="https://via.placeholder.com/50" alt="key" className="key__icon" />
+                        <p>New/Manage Layers</p>
+                    </li>
+                    <li className="key__li">
+                        <img src="https://via.placeholder.com/50" alt="key" className="key__icon" />
+                        <p>Toggle Dark/Light Mode</p>
+                    </li>
+                    <li className="key__li">
+                        <img src="https://via.placeholder.com/50" alt="key" className="key__icon" />
+                        <p>Clear Canvas</p>
+                    </li>
+                    <li className="key__li">
+                        <img src="https://via.placeholder.com/50" alt="key" className="key__icon" />
+                        <p>Home</p>
+                    </li>
+                    <li className="key__li">
+                        <img src="https://via.placeholder.com/50" alt="key" className="key__icon" />
+                        <p>About</p>
+                    </li>
+                </ul>
+                </div>
+
             
+                <div className="nav__link--canvas">
+                <Link to="/canvas" className="nav__link">
+                    <img src={back} alt="back button" className="nav__icon" />
+                    <h3 className="nav__h3">Back to Canvas</h3>
+                </Link>
+                </div>
+                <div className="nav__link--home">
+                <Link to="/">
+                    <img src={home} alt="home button" className="nav__icon" />
+                </Link>
+                </div>
         </div>
     )
 }
